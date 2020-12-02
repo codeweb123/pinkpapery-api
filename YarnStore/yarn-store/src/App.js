c21ed4b568a5d50229b0fafe6c92b072a608bbed
codeweb123 <jiyonj@hotmail.com>
2020-12-01 21:28:09 -0500

    initial commit

---
+++ YarnStore/yarn-store/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './homepage';
+
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ YarnStore/yarn-store/src/homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='three-imgs'>
+        </div>
+        <div className='header'>
+            <div className='content'>
+            </div>
+        </div>
+        <div className='featured'>
+            <div className='featured-item'>
+                <div className='item-info'>
+                    <h1 className='title'>Blanket</h1>
+                    <span className='subtitle'>Buy Me</span>
+                </div>
+            </div>
+        </div>
+        <div className='all-items'>
+            <div className='items'>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
