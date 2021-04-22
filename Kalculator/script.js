01f7a4bbd4d84bd5142d32f1c766f1e5614bfb5d
codeweb123 <jiyonj@hotmail.com>
2021-04-21 23:27:41 -0400

    update

---
+++ Kalculator/script.js
@@ -0,0 +1,21 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+}
+
+//event listeners
+inputBtns.forEach((inputBtn) => {
+    //class equals 0
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    }
+});
+
