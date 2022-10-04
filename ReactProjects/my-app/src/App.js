ec7eb59284d7f99ca82b70d29754baca1d66edb3
codeweb123 <jiyonj@hotmail.com>
2022-10-04 13:43:44 -0500

    update

--- ReactProjects/my-app/src/App.js
+++ ReactProjects/my-app/src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -20,23 +20,33 @@ class App extends Component {
             return { monsters: users };
           },
           () => {
-            {
-              this.state;
-            }
+            console.log(this.state);
           }
         )
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
           className="search-box"
           type="search"
           placeholder="search monsters"
+          onChange={this.onSearchChange}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
