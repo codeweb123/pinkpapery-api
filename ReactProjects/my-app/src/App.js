36a2323971b8e326a44d336c48601b1570accbbe
codeweb123 <jiyonj@hotmail.com>
2022-10-04 16:16:41 -0500

    update

--- ReactProjects/my-app/src/App.js
+++ ReactProjects/my-app/src/App.js
@@ -34,8 +34,11 @@ class App extends Component {
   };
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -44,7 +47,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
