1e425ec430f9e8238e32f7229c74790375de4cec
codeweb123 <jiyonj@hotmail.com>
2022-10-11 10:25:00 -0500

    add card list style

--- ReactProjects/my-app/src/components/card-list/card-list.component.jsx
+++ ReactProjects/my-app/src/components/card-list/card-list.component.jsx
@@ -6,7 +6,10 @@ class CardList extends Component {
         return (
             <div className='card-list'>
                 {monsters.map((monster) => (
-                    <div className='card-container'>
+                    <div className='card-container' key={monster.id}>
+                        <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+                        <h2>{monster.name}</h2>
+                        <p>{monster.email}</p>
                     </div>
                 ))}
             </div>
