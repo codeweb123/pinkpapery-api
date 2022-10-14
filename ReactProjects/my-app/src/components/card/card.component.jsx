a449505f1ea766b1ea655b8b30a6a5dca85de883
codeweb123 <jiyonj@hotmail.com>
2022-10-14 11:50:52 -0500

    updtae

--- ReactProjects/my-app/src/components/card/card.component.jsx
+++ ReactProjects/my-app/src/components/card/card.component.jsx
@@ -1,18 +1,34 @@
-import { Component } from 'react';
 import './card.styles.css';
 
-class Card extends Component {
-    render() {
-        const {id, name, email} = this.props.monster;
-        return (
-            <div className='card-container' key={id}>
-            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}>
+const Card = ({monster}) => {
+    const {id, name, email} = monster;
+    
+    return (
+        <div className='card-container' key={id}>
+            <img 
+                alt={`monster ${name}`} 
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}>
             </img>
             <h2>{name}</h2>
             <p>{email}</p>
-            </div>
+        </div>
         )
     }
-}
 
 export default Card;
+
+// class Card extends Component {
+//     render() {
+//         const {id, name, email} = this.props.monster;
+//         return (
+//             <div className='card-container' key={id}>
+//             <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}>
+//             </img>
+//             <h2>{name}</h2>
+//             <p>{email}</p>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Card;
