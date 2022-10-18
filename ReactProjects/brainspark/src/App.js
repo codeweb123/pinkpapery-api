29720d98413e3590f493ddc591e2af463c5648ec
codeweb123 <jiyonj@hotmail.com>
2022-10-18 13:56:16 -0500

    create App.js

--- ReactProjects/brainspark/src/App.js
+++ ReactProjects/brainspark/src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Reading",
+    },
+    {
+      id: 2,
+      title: "Math",
+    },
+    {
+      id: 3,
+      title: "Science",
+    },
+    {
+      id: 4,
+      title: "Social Studies",
+    },
+    {
+      id: 5,
+      title: "Writing",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
