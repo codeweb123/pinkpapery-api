62b3e1ae77c04158e35bb3354b1399359980b52c
codeweb123 <jiyonj@hotmail.com>
2022-10-25 11:41:55 -0500

    add firestore

--- ReactProjects/brainspark/src/routes/sign-in/sign-in.component.jsx
+++ ReactProjects/brainspark/src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,16 @@
-import { signInWithGooglePopup } from ' ../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const {user} = await signInWithGooglePopup();
+        createUserDocumentFromAuth(user)
     }
 
     return (
         <div>
         <h1>Sign In Page
         </h1>
-        <button> Sign In with Google Popup </button>
+        <button onClick={logGoogleUser}> Sign In with Google Popup </button>
         </div>
     )
 }
