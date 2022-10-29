bdb2708354295f2bfead84bb7f200b1b65b3bbc5
codeweb123 <jiyonj@hotmail.com>
2022-10-29 07:45:07 -0500

    update

--- ReactProjects/brainspark/src/routes/navigation/navigation.component.jsx
+++ ReactProjects/brainspark/src/routes/navigation/navigation.component.jsx
@@ -6,7 +6,6 @@ import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
 //|| Fragment used if you don't want to render out a specific html element. Example: Use instead of a container div.
 //|| Outlet renders out <Routes> </Routes>
-
 const Navigation = () => {
     return (
       <Fragment>
