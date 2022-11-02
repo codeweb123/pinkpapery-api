40081c8e45967b81cbe36c35da9eaa7061f65c7c
codeweb123 <jiyonj@hotmail.com>
2022-11-01 20:18:16 -0500

    update

--- ReactProjects/brainspark/src/contexts/user.context.jsx
+++ ReactProjects/brainspark/src/contexts/user.context.jsx
@@ -1,5 +1,16 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
+//actual value you want to access
 export const UserContext = createContext({
 
 })
+
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    
+    return <UserContext.Provider value={}>{children}</UserContext.Provider>
+}
+
+<UserProvider>
+    <app />
+</UserProvider>
