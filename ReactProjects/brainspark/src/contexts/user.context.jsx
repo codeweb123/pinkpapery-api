11cab64e0b605a2b12c6a720007eaa2dd28e6fe2
codeweb123 <jiyonj@hotmail.com>
2022-11-01 21:05:34 -0500

    add UserProvider to index.js

--- ReactProjects/brainspark/src/contexts/user.context.jsx
+++ ReactProjects/brainspark/src/contexts/user.context.jsx
@@ -2,14 +2,16 @@ import { createContext, useState } from 'react'
 
 //context-actual value you want to access
 export const UserContext = createContext({
-
-})
+    currentUser: null, 
+    setCurrentUser: () => null,
+});
 
 //provider- actual component
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
+    const value = { currentUser, setCurrentUser };
 
-    return <UserContext.Provider value={}>{children}</UserContext.Provider>
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
 
 //wrap provider around app
