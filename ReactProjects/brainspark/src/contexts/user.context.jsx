08112a7f7fbc594e0e532a8c5f364bcb0f19d8c3
codeweb123 <jiyonj@hotmail.com>
2022-11-01 20:24:08 -0500

    add context and provider

--- ReactProjects/brainspark/src/contexts/user.context.jsx
+++ ReactProjects/brainspark/src/contexts/user.context.jsx
@@ -1,16 +1,18 @@
 import { createContext, useState } from 'react'
 
-//actual value you want to access
+//context-actual value you want to access
 export const UserContext = createContext({
 
 })
 
+//provider- actual component
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
-    
+
     return <UserContext.Provider value={}>{children}</UserContext.Provider>
 }
 
+//wrap provider around app
 <UserProvider>
     <app />
 </UserProvider>
