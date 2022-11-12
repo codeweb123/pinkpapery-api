ec8e3afe18d19e6b7b0232f46cfbc152a4432b0d
codeweb123 <jiyonj@hotmail.com>
2022-11-11 21:00:57 -0600

    add logo and link

--- ReactProjects/brainspark/src/components/directory/directory.component.jsx
+++ ReactProjects/brainspark/src/components/directory/directory.component.jsx
@@ -3,7 +3,6 @@ import DirectoryItem from "../directory-item/directory-item.component";
 import './directory.styles.scss';
 
 const Directory = ({categories}) => {
-
     return (
         <div className="directory-container">
             {categories.map((category) => (
