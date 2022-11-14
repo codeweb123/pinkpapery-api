e512dc2a1b0494e00135a4aa16eb5b72bd066823
codeweb123 <jiyonj@hotmail.com>
2022-11-14 16:23:10 -0600

    updating user context, reducer, provider

--- ReactProjects/brainspark/src/contexts/user.context.jsx
+++ ReactProjects/brainspark/src/contexts/user.context.jsx
@@ -5,9 +5,34 @@ export const UserContext = createContext({
     currentUser: null, 
     setCurrentUser: () => null,
 });
+
+export const
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+    
+    switch(type) {
+        case 'SET_CURRENT_USER':
+         return {
+            ...state,
+            currentUser: payload
+         }
+        default: 
+         throw new Error(`Unhandled type ${type} in userReducer `)
+    }
+    
+}
+
+
 //provider- actual component
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+
+    const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
