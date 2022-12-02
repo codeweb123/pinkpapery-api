538498cd125ec952b897f686dc17e0f8b5c99ac9
codeweb123 <jiyonj@hotmail.com>
2022-12-02 13:52:44 -0600

    add js fuctionality

---
+++ Downloads/-- template-light-dark-mode/script.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+//Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / )";
+}
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme);
