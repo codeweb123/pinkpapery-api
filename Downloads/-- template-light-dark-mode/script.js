38e0b2835032679c505834401d7a445a4da0ab75
codeweb123 <jiyonj@hotmail.com>
2022-12-02 21:50:11 -0600

    add dark mode toggle

--- Downloads/-- template-light-dark-mode/script.js
+++ Downloads/-- template-light-dark-mode/script.js
@@ -9,7 +9,10 @@ const textBox = document.getElementById("text-box");
 //Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = "rgb(255 255 255 / )";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
 }
 
 function switchTheme(event) {
