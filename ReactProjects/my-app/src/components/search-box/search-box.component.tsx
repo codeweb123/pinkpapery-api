2e9df9c495cf26ef18e12536b14ffadfd7766ada
codeweb123 <jiyonj@hotmail.com>
2022-12-05 22:54:45 -0600

    add interface and function in TS

--- ReactProjects/my-app/src/components/search-box/search-box.component.tsx
+++ ReactProjects/my-app/src/components/search-box/search-box.component.tsx
@@ -1,4 +1,13 @@
+import { type } from '@testing-library/user-event/dist/type';
 import './search-box.styles.css'
+
+interface ISearchBoxProps {
+    className: string;
+    placeholder?: string | null;
+}
+
+
+const func: (a: string, b: number, c: boolean) => = (void) => {}
 
 const SearchBox = ({className, placeholder, onChangeHandler }) => (    <input
     className={`search-box ${className}`}
