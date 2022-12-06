e97571e7922f84412fee8f46bbb2e5aad5a87831
codeweb123 <jiyonj@hotmail.com>
2022-12-06 14:38:36 -0600

    add TS

--- ReactProjects/my-app/src/components/search-box/search-box.component.tsx
+++ ReactProjects/my-app/src/components/search-box/search-box.component.tsx
@@ -3,13 +3,41 @@ import './search-box.styles.css'
 
 interface ISearchBoxProps {
     className: string;
-    placeholder?: string | null;
+    placeholder?: string;
 }
+
+interface ISearchBoxProps {
+    onChangeHandler: (a: string) => void
+}
+
+type SearchBoxProps = {
+    className: string;
+    placeholder?: string;
+    onChangeHandler: (a: string) => void;
+}
+
+type CanadianAddress = {
+    street: string;
+    province: string;
+}
+
+type USAddress = {
+    street: string;
+    state: string;
+}
+
+const Address = {
+    street: '',
+    state: '',
+    province: ''
+}
+
+
 
 
 const func: (a: string, b: number, c: boolean) => = (void) => {}
 
-const SearchBox = ({className, placeholder, onChangeHandler }) => (    <input
+const SearchBox = ({className, placeholder, onChangeHandler }: ISearchBoxProps) => (    <input
     className={`search-box ${className}`}
     type='search'
     placeholder={placeholder}
