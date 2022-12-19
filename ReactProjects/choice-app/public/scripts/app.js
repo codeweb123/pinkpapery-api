9ed82a2f19d535e3718f45b03c70ca38ca674cfc
codeweb123 <jiyonj@hotmail.com>
2022-12-19 16:39:18 -0600

    add JSX

--- ReactProjects/choice-app/public/scripts/app.js
+++ ReactProjects/choice-app/public/scripts/app.js
@@ -1,1 +1,22 @@
+"use strict";
+
 console.log("App.js is running");
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Choice App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info."
+  )
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
