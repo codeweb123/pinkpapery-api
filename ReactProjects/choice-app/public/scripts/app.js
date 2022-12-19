5a72f025e5bd1b72da0ea7118c158f7a94949718
codeweb123 <jiyonj@hotmail.com>
2022-12-19 17:27:38 -0600

    add JSX object

--- ReactProjects/choice-app/public/scripts/app.js
+++ ReactProjects/choice-app/public/scripts/app.js
@@ -14,9 +14,55 @@ var template = React.createElement(
     "p",
     null,
     "This is some info."
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
+);
+
+var user = {
+  name: "Jiyon",
+  age: 42,
+  location: "Sugar Land, TX"
+};
+
+var userName = "Jiyon";
+var userAge = 42;
+var userLocation = "Sugar Land, TX";
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
   )
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- ReactProjects/choice-app/src/app.js
+++ ReactProjects/choice-app/src/app.js
@@ -4,9 +4,31 @@ var template = (
   <div>
     <h1>Choice App</h1>
     <p>This is some info.</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: "Jiyon",
+  age: 42,
+  location: "Sugar Land, TX",
+};
+
+var userName = "Jiyon";
+var userAge = 42;
+var userLocation = "Sugar Land, TX";
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
   </div>
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
