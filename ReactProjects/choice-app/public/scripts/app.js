8537810f9cef8444889f22b08893a8163508976c
codeweb123 <jiyonj@hotmail.com>
2022-12-23 00:38:22 -0600

    update

--- ReactProjects/choice-app/public/scripts/app.js
+++ ReactProjects/choice-app/public/scripts/app.js
@@ -1,76 +1,11 @@
 "use strict";
 
-console.log("App.js is running!!!!!");
-
-var app = {
-  title: "Choice App!",
-  subtitle: "This is some info!"
+var square = function square(x) {
+  return x * x;
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Item one"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item two"
-    )
-  )
-);
-
-var user = {
-  name: "Jiyon",
-  age: "42",
-  location: "Sugar Land"
+var squareArrow = function squareArrow(x) {
+  return x * x;
 };
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      "p",
-      null,
-      " Location: ",
-      location
-    );
-  }
-}
-
-var templateTwo = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    user.name ? user.name : "anonymous"
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age,
-    " "
-  ),
-  getLocation(user.location)
-);
-
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+console.log(squareArrow(8));
