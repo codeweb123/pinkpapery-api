84b5332f3b0fd07e42e7068f86073f7a1c504adf
codeweb123 <jiyonj@hotmail.com>
2023-01-02 18:02:54 -0600

    add template

--- ReactProjects/your-choice/public/scripts/app.js
+++ ReactProjects/your-choice/public/scripts/app.js
@@ -1,3 +1,6 @@
 console.log("App.js is running!");
 
-var template = true;
+var template = <p> This is JSX from app.js! </p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
