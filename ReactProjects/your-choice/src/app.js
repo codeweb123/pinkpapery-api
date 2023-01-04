016c0c10b5cb609cf00529a37334fb75a814c8b2
codeweb123 <jiyonj@hotmail.com>
2023-01-03 20:05:13 -0600

    update

--- ReactProjects/your-choice/src/app.js
+++ ReactProjects/your-choice/src/app.js
@@ -1,7 +1,6 @@
-"use strict";
-
 console.log("App.js is running!");
 
+var template = <p>This is JSX from app.js!</p>;
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
