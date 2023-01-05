3caf4a726b5eef9f906f7279b37888acc5263700
codeweb123 <jiyonj@hotmail.com>
2023-01-05 11:40:25 -0600

    update

--- ReactProjects/your-choice/public/scripts/app.js
+++ ReactProjects/your-choice/public/scripts/app.js
@@ -2,6 +2,24 @@
 
 console.log("App.js is running!");
 
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Jiyon Trammell"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 42"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Sugar Land"
+  )
+);
 var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- ReactProjects/your-choice/src/app.js
+++ ReactProjects/your-choice/src/app.js
@@ -1,6 +1,11 @@
 console.log("App.js is running!");
 
-var template = <p>This is JSX from app.js!</p>;
+var templateTwo = (
+  <div>
+    <h1>Jiyon Trammell</h1>
+    <p>Age: 42</p>
+    <p>Location: Sugar Land</p>
+  </div>
+);
 var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
