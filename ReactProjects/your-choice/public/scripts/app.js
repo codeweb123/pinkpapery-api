34d9babfb886f2c1cd08c5b2b546c861fd4828b6
codeweb123 <jiyonj@hotmail.com>
2023-01-05 12:23:46 -0600

    add object

--- ReactProjects/your-choice/public/scripts/app.js
+++ ReactProjects/your-choice/public/scripts/app.js
@@ -2,24 +2,38 @@
 
 console.log("App.js is running!");
 
+var user = {
+  name: "Andrew",
+  age: 26,
+  location: "Philly"
+};
+
+var userName = "Maddie";
+var userAge = 27;
+var userLocation = "Phily";
+
 var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    "Jiyon Trammell"
+    user.name
   ),
   React.createElement(
     "p",
     null,
-    "Age: 42"
+    "Age: ",
+    user.age
   ),
   React.createElement(
     "p",
     null,
-    "Location: Sugar Land"
+    "Location: ",
+    user.location,
+    " "
   )
 );
+
 var appRoot = document.getElementById("app");
 ReactDOM.render(templateTwo, appRoot);
--- ReactProjects/your-choice/src/app.js
+++ ReactProjects/your-choice/src/app.js
@@ -1,11 +1,22 @@
 console.log("App.js is running!");
 
+var user = {
+  name: "Andrew",
+  age: 26,
+  location: "Philly",
+};
+
+var userName = "Maddie";
+var userAge = 27;
+var userLocation = "Phily";
+
 var templateTwo = (
   <div>
-    <h1>Jiyon Trammell</h1>
-    <p>Age: 42</p>
-    <p>Location: Sugar Land</p>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location} </p>
   </div>
 );
+
 var appRoot = document.getElementById("app");
 ReactDOM.render(templateTwo, appRoot);
