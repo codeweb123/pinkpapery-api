ddbe422a2fbc148fe51eb754ec70b35b35f01efb
codeweb123 <jiyonj@hotmail.com>
2023-01-10 11:22:13 -0600

    added error message and styling if passwords do not match

--- Downloads/form-validation/script.js
+++ Downloads/form-validation/script.js
@@ -10,12 +10,24 @@ let passwordsMatch = false;
 //.checkValidity method associated with form element.
 function validateForm() {
   isValid = form.checkValidity();
-  console.log(isValid);
 
   if (!isValid) {
     message.textContent = "Please fill out all fields.";
     message.style.color = "red";
     messageContainer.style.borderColor = "red";
+  }
+  //check to see if passwords match
+  if (password1.value === password2.value) {
+    passwordsMatch = true;
+    password1.style.borderColor = "green";
+    password2.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1.style.borderColor = "red";
+    password2.style.borderColor = "red";
   }
 }
 
