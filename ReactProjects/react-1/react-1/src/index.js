4a79c0324bdab0ae1df74ac8fe24ce80a75674f4
codeweb123 <jiyonj@hotmail.com>
2023-01-12 12:06:32 -0600

    add css

--- ReactProjects/react-1/react-1/src/index.js
+++ ReactProjects/react-1/react-1/src/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const Greeting = () => {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL600_SR600,400_.jpg"
+    alt="Spare"
+  />
+);
+const Title = () => <h2>Spare</h2>;
+const Author = () => <h4>Prince Harry The Duke of Sussex </h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
