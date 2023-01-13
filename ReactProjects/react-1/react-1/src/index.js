a9c316badd2a1e1a5b45f668dbbde84ec32c30f5
codeweb123 <jiyonj@hotmail.com>
2023-01-13 11:14:56 -0600

    add key in BookList

--- ReactProjects/react-1/react-1/src/index.js
+++ ReactProjects/react-1/react-1/src/index.js
@@ -2,31 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  title: "Spare",
-  author: "Prince Harry The Duke of Sussex",
-  img: "./images/book-1.jpg",
-};
+const books = [
+  {
+    title: "Spare",
+    author: "Prince Harry The Duke of Sussex",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title: "James Clear",
+    author: "Atomic Habits",
+    img: "./images/book-1.jpg",
+    id: 2,
+  },
+];
 
-const secondBook = {
-  title: "James Clear",
-  author: "Atomic Habits",
-  img: "./images/book-1.jpg",
-};
+//const names = ["jo", "su", "fe"];
+//const newNames = names.map((name) => {
+//  return <h1>{name}</h1>;
+//});
+//console.log(newNames);
+//place key - usually id in Main Return where you render out the component. Usually the "mainList".
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id} />;
+      })}
     </section>
   );
 };
