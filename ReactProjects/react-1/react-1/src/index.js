b6bf17690063e0eaea5da6eaba70fd41118a2cca
codeweb123 <jiyonj@hotmail.com>
2023-01-13 10:16:27 -0600

    add destructuring syntax in function parameters

--- ReactProjects/react-1/react-1/src/index.js
+++ ReactProjects/react-1/react-1/src/index.js
@@ -2,22 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const title = "Spare";
-const author = "Prince Harry The Duke of Sussex";
-const img = "./images/book-1.jpg";
+const firstBook = {
+  title: "Spare",
+  author: "Prince Harry The Duke of Sussex",
+  img: "./images/book-1.jpg",
+};
+
+const secondBook = {
+  title: "James Clear",
+  author: "Atomic Habits",
+  img: "./images/book-1.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
