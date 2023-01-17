3a4c858c94af0caeb4955600ef4efe444845d705
codeweb123 <jiyonj@hotmail.com>
2023-01-16 21:42:54 -0600

    add images

---
+++ ReactProjects/react-1/react-1/src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    title: "Spare",
+    author: "Prince Harry The Duke of Sussex",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "./images/book-2.jpg",
+    id: 2,
+  },
+  {
+    title: "Valentine's Day",
+    author: "Alice Schertle",
+    img: "./images/vday.jpg",
+    id: 3,
+  },
+];
--- ReactProjects/react-1/react-1/src/index.js
+++ ReactProjects/react-1/react-1/src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    title: "Spare",
-    author: "Prince Harry The Duke of Sussex",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    title: "Atomic Habits",
-    author: "James Clear",
-    img: "./images/book-1.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import { Book } from "./Book";
 
 //const names = ["jo", "su", "fe"];
 //const newNames = names.map((name) => {
@@ -25,16 +12,12 @@ const books = [
 //place key - usually id in Main Return where you render out the component. Usually the "mainList".
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
     <section className="booklist">
       <EventExamples />
 
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
@@ -79,20 +62,6 @@ const EventExamples = () => {
         </div>
       </form>
     </section>
-  );
-};
-
-const Book = ({ img, title, author, getBook, id }) => {
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>click me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
