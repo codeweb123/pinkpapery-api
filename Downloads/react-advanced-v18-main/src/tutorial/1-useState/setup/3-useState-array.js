97e9c34152f42c0ee0a6afe98c7b2b0920792386
codeweb123 <jiyonj@hotmail.com>
2023-01-23 13:14:39 -0600

    add useState hook with Array practice

--- Downloads/react-advanced-v18-main/src/tutorial/1-useState/setup/3-useState-array.js
+++ Downloads/react-advanced-v18-main/src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,12 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
   return (
     <React.Fragment>
       {people.map((person) => {
@@ -10,9 +16,13 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
     </React.Fragment>
   );
 };
