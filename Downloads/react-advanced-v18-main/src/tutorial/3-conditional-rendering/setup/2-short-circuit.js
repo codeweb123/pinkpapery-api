3b5e6127e8b5d58486031fffa8de3546bf8d6a18
codeweb123 <jiyonj@hotmail.com>
2023-01-30 12:03:27 -0600

    add ternary operator

--- Downloads/react-advanced-v18-main/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ Downloads/react-advanced-v18-main/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
-
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+  return (
+    <React.Fragment>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default ShortCircuit;
