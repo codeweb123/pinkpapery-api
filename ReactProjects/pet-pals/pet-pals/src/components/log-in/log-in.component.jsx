a3c8652c004fcf3ec11f61bf947255cda10ac850
codeweb123 <jiyonj@hotmail.com>
2023-02-03 10:05:33 -0600

    update

--- ReactProjects/pet-pals/pet-pals/src/components/log-in/log-in.component.jsx
+++ ReactProjects/pet-pals/pet-pals/src/components/log-in/log-in.component.jsx
@@ -2,10 +2,12 @@ import PetItem from "../pet-item/pet-item.component";
 
 const Login = ({ pets }) => {
   return (
-    <div className="pets-choice-container">
+    <div className="login-container">
       {pets.map((pet) => (
         <PetItem key={pet.id} pet={pet} />
       ))}
     </div>
   );
 };
+
+export default Login;
