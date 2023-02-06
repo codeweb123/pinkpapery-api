d794e7d6b4e115389a1357f3b76109762d05db63
codeweb123 <jiyonj@hotmail.com>
2023-02-06 14:02:27 -0600

    scss styling to navigation

--- ReactProjects/pet-pals/pet-pals/src/routes/navigation/navigation.component.jsx
+++ ReactProjects/pet-pals/pet-pals/src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1> I am the navigation bar!</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={Logo} alt="logo"></img>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/">
+            Login
+          </Link>
+          <Link className="nav-link" to="/land">
+            Pick_a_Land
+          </Link>
+          <Link className="nav-link" to="/pet">
+            Pick_a_Pet
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
