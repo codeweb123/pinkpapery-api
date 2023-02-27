9783ca4c273376d2c77c25a2c0a0f1c6b3fec6d9
codeweb123 <jiyonj@hotmail.com>
2023-02-27 13:08:15 -0600

    add math problem equations to DOM

--- ReactProjects/pet-pals/pet-pals/src/components/math-problem-box/math-problem-box.component.jsx
+++ ReactProjects/pet-pals/pet-pals/src/components/math-problem-box/math-problem-box.component.jsx
@@ -2,6 +2,61 @@ import "./math-problem-box.styles.scss";
 import Button from "../button/button.component";
 
 const MathProblemBox = () => {
+  let questionAmount = 10;
+  let equationsArray = [];
+  let firstNumber = 0;
+  let secondNumber = 0;
+  let equationObject = {};
+  const wrongFormat = [];
+
+  const correctEquations = getRandomInt(questionAmount);
+  const wrongEquations = questionAmount - correctEquations;
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = getRandomInt(9);
+    secondNumber = getRandomInt(9);
+
+    const equationValue = firstNumber * secondNumber;
+    const equation = `${firstNumber} X ${secondNumber} = ${equationValue}`;
+    equationObject = { value: equation, evaluated: "true" };
+    equationsArray.push(equationObject);
+
+    for (let i = 0; i < wrongEquations; i++) {
+      firstNumber = getRandomInt(9);
+      secondNumber = getRandomInt(9);
+      const equationValue = firstNumber * secondNumber;
+      wrongFormat[0] = `${firstNumber} X ${
+        secondNumber + 1
+      } = ${equationValue}`;
+      wrongFormat[1] = `${firstNumber} X ${secondNumber} = ${
+        equationValue - 1
+      }`;
+      wrongFormat[2] = `${
+        firstNumber + 1
+      } X ${secondNumber} = ${equationValue}`;
+      const formatChoice = getRandomInt(3);
+      const equation = wrongFormat[formatChoice];
+      equationObject = { value: equation, evaluated: "false" };
+      equationsArray.push(equationObject);
+    }
+    equationsToDOM();
+  }
+
+  function equationsToDOM() {
+    equationsArray.forEach((equation) => {
+      const item = document.createElement("div");
+      item.classList.add("item");
+      const equationText = document.createElement("h1");
+      equationText.textContent = equation.value;
+      item.appendChild(equationText);
+      itemContainer.appendChild(item);
+    });
+  }
+
   return (
     <div className="game-container">
       <div className="header">
@@ -11,15 +66,8 @@ const MathProblemBox = () => {
         <h1 className="countdown">3</h1>
       </div>
       <div className="card" id="game-page">
-        <div className="item-container">
-          <div className="selected-item"></div>
-          <div className="item">1 + 1 = 2</div>
-          <div className="item">1 + 2 = 3</div>
-        </div>
-        <div className="item-footer">
-          <Button className="wrong">Wrong</Button>
-          <Button className="right">Right</Button>
-        </div>
+        <div className="selected-item"></div>
+        <div className="itemContainer"></div>
       </div>
       <div className="card" id="score-page" hidden>
         <div className="score-container">
